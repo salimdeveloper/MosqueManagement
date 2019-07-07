b93e823f5ae19ee6f6b9b14117f87965d2613d7a
SalimAlam <salimalamdeveloper@gmail.com>
2019-07-07 16:29:49 +0000

    Created UnitOfWork.cs

---
+++ MosqueManagement.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using MosqueManagement.Core.Interfaces;
+using MosqueManagement.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MosqueManagement.Infrastructure
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public IAddressRepository Addresses { get; private set; }
+        public IMemberRepository Members { get; private set; }
+        public IMemberTypeRepository MemberTypes { get; private set; }
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Addresses = new AddressRepository(context);
+        }
+        public void Complete()
+        {
+            _context.SaveChanges();
+        }
+
+        public void CompleteAsync()
+        {
+            _context.SaveChangesAsync();
+        }
+    }
+}
