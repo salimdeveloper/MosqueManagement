49f2d0be79f86459bf3ede01c0a1d2074aa92bb1
SalimAlam <salimalamdeveloper@gmail.com>
2019-07-07 16:30:37 +0000

    Implemented the AddressRepository

--- MosqueManagement.Core/Interfaces/IMemberRepository.cs
+++ MosqueManagement.Core/Interfaces/IMemberRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace MosqueManagement.Core.Interfaces
+﻿using MosqueManagement.Core.Models;
+
+namespace MosqueManagement.Core.Interfaces
 {
     public interface IMemberRepository
     {
+        void AddMember(Member member);
+
     }
 }
---
+++ MosqueManagement.Infrastructure/Repositories/AddressRepository.cs
@@ -0,0 +1,22 @@
+﻿using MosqueManagement.Core.Interfaces;
+using MosqueManagement.Core.Models;
+using MosqueManagement.Infrastructure.Data;
+using System.Linq;
+
+namespace MosqueManagement.Infrastructure
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private ApplicationDbContext _context;
+
+        public AddressRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public Address GetAddress(int id)
+        {
+            return _context.Addresses.Where(a => a.Id == id).SingleOrDefault();
+        }
+    }
+}
